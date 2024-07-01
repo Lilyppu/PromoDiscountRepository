f8e0cac71cd3feea65053119e691e8a7b38e8e1f
Lilyppu <lily@ppu.co.id>
2024-07-01 11:52:25 +0700

    first commit

---
+++ src/main/java/com/PromoDiscount/PromoDiscountApplication.java
@@ -0,0 +1,13 @@
+package com.PromoDiscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoDiscountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoDiscountApplication.class, args);
+	}
+
+}
